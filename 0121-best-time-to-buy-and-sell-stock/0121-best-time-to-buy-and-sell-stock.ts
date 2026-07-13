ccbbc2f3e542602c7e1c4bcc08393388f5d69862
QUDDUS <qudduslarek@proton.me>
2026-07-13 22:16:34 +0500

    Time: 3 ms (53.34%), Space: 65.6 MB (74.92%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,13 @@
+function maxProfit(prices: number[]): number {
+
+    let minPrice: number = prices[0]; 
+    let maxProfit: number = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i]);
+    
+        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    }
+
+    return maxProfit;
+};
